d296bf940d5ac6aa7042db3bff9a3fe4a5be421b
Juan Manuel Gutierrez Monaco <juan.monaco.gutierrez@gmail.com>
2024-01-25 09:42:30 -0300

    Theater CRUD finished - calling the GET with id correctly

--- routes/route.go
+++ routes/route.go
@@ -9,9 +9,14 @@ func BuildRoutes(router *mux.Router) {
 	// Theater Routes
 	router.HandleFunc("/api/theater", controllers.CreateTheater).Methods("POST")
 	router.HandleFunc("/api/theater", controllers.GetTheaters).Methods("GET")
-	router.HandleFunc("/api/theater/{id}", controllers.GetTheaters).Methods("GET")
+	router.HandleFunc("/api/theater/{id}", controllers.GetTheaterByID).Methods("GET")
 	router.HandleFunc("/api/theater/{id}", controllers.UpdateTheater).Methods("PUT")
 	router.HandleFunc("/api/theater/{id}", controllers.DeleteTheater).Methods("DELETE")
+
+	// Screening Routes
+	router.HandleFunc("/api/screening", controllers.CreateScreening).Methods("POST")
+	router.HandleFunc("/api/theater", controllers.GetScreenings).Methods("GET")
+	router.HandleFunc("/api/theater/{id}", controllers.GetTheaters).Methods("GET")
 	router.HandleFunc("/api/theater/{id}", controllers.UpdateTheater).Methods("PUT")
 	router.HandleFunc("/api/theater/{id}", controllers.DeleteTheater).Methods("DELETE")
 }
